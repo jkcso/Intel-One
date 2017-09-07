36ab894e2bbaafa306e2a0fc98059c8d79d1c46b
jkatsioloudes <jk2714@ic.ac.uk>
2017-09-07 14:05:16 +0300

    Refactors social media function search.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 # This class is the main thread of the program, includes the welcome message function and the main function.
-# External imports
-
 # Internal imports
 import intro
 import help
 import search.google as google
-import search.socialMedia as socialMedia
+import search.socialMedia as social
 import search.pipl as pipl
 import search.edgar as edgar
 import search.whois as whois
@@ -21,74 +19,74 @@ if __name__ == '__main__':
     # the main loop of the program running constantly until ctl+C or exit/quit is used.
     while True:
         # holds the current command of the user.
-        userCommand = str(input('who-dis> '))
+        userQuery = str(input('who-dis> '))
 
         # goes to help.
-        if userCommand == 'help' or userCommand == '-h' or userCommand == '--help':
+        if userQuery == 'help' or userQuery == '-h' or userQuery == '--help':
             help.help()
 
         # performs google search.
-        elif userCommand.__contains__('-g') or userCommand.__contains__('--google'):
-            google.googleSearch(userCommand)
+        elif userQuery.__contains__('-g') or userQuery.__contains__('--google'):
+            google.googleSearch(userQuery)
 
         # performs facebook search.
-        elif userCommand.__contains__('-fb') or userCommand.__contains__('--facebook'):
-            socialMedia.facebookSearch(userCommand)
+        elif userQuery.__contains__('-fb') or userQuery.__contains__('--facebook'):
+            social.SocialMedia.retrieveAccounts(userQuery, 'fb')
 
         # performs linkedin search.
-        elif userCommand.__contains__('-ln') or userCommand.__contains__('--linkedin'):
-            socialMedia.linkedinSearch(userCommand)
+        elif userQuery.__contains__('-ln') or userQuery.__contains__('--linkedin'):
+            social.SocialMedia.retrieveAccounts(userQuery, 'ln')
 
         # performs twitter search.
-        elif userCommand.__contains__('-tw') or userCommand.__contains__('--twitter'):
-            socialMedia.twitterSearch(userCommand)
+        elif userQuery.__contains__('-tw') or userQuery.__contains__('--twitter'):
+            social.SocialMedia.retrieveAccounts(userQuery, 'tw')
 
         # performs instagram search.
-        elif userCommand.__contains__('-in') or userCommand.__contains__('--instagram'):
-            socialMedia.instagramSearch(userCommand)
+        elif userQuery.__contains__('-in') or userQuery.__contains__('--instagram'):
+            social.SocialMedia.retrieveAccounts(userQuery, 'in')
 
         # performs reddit search.
-        elif userCommand.__contains__('-re') or userCommand.__contains__('--reddit'):
-            socialMedia.redditSearch(userCommand)
+        elif userQuery.__contains__('-re') or userQuery.__contains__('--reddit'):
+            social.SocialMedia.retrieveAccounts(userQuery, 're')
 
         # performs search about posts on the given keyword in social search website looking in all available social media.
-        elif userCommand.__contains__('-ss') or userCommand.__contains__('--social'):
-            socialMedia.SocialSearcher.ssSearch(userCommand)
+        # elif userQuery.__contains__('-ss') or userQuery.__contains__('--social'):
+        #     social.SocialMedia.retrievePosts(userQuery)
 
         # performs a search in www.pipl.com containing location information as well to be more specific.
-        elif userCommand.__contains__('-p') and userCommand.__contains__('-l'):
-            pipl.piplSearchLocation(userCommand)
+        elif userQuery.__contains__('-p') and userQuery.__contains__('-l'):
+            pipl.piplSearchLocation(userQuery)
 
         # performs a search in www.pipl.com to capture the social media not captured above.
-        elif userCommand.__contains__('-p') or userCommand.__contains__('--pipl'):
-            pipl.piplSearch(userCommand)
+        elif userQuery.__contains__('-p') or userQuery.__contains__('--pipl'):
+            pipl.piplSearch(userQuery)
 
         # provides a link to search for companies in www.sec.gov.
-        elif userCommand.__contains__('-e') or userCommand.__contains__('--edgar'):
-            edgar.edgarSearch(userCommand)
+        elif userQuery.__contains__('-e') or userQuery.__contains__('--edgar'):
+            edgar.edgarSearch(userQuery)
 
         # provides a link to search for target domains in who.is website.
-        elif userCommand.__contains__('-w') or userCommand.__contains__('--whois'):
-            whois.whoIsSearch(userCommand)
+        elif userQuery.__contains__('-w') or userQuery.__contains__('--whois'):
+            whois.whoIsSearch(userQuery)
 
         # provides a link to search in shodan.io.
-        elif userCommand.__contains__('-sh') or userCommand.__contains__('--shodan'):
-            shodan.shodanSearch(userCommand)
+        elif userQuery.__contains__('-sh') or userQuery.__contains__('--shodan'):
+            shodan.shodanSearch(userQuery)
 
         # provides a link to search in asafaweb website for vulnerability scanning.
-        elif userCommand.__contains__('-sc') or userCommand.__contains__('--scan'):
-            scanner.scanSearch(userCommand)
+        elif userQuery.__contains__('-sc') or userQuery.__contains__('--scan'):
+            scanner.scanSearch(userQuery)
 
         # # Uses reddit username to get insights on lifetime reddit activity.
-        elif userCommand.__contains__('-reU') or userCommand.__contains__('--redditUser'):
-            reddit.redditSearch(userCommand)
+        elif userQuery.__contains__('-reU') or userQuery.__contains__('--redditUser'):
+            reddit.redditSearch(userQuery)
 
         # exits the program.
-        elif userCommand == 'exit' or userCommand == 'quit':
+        elif userQuery == 'exit' or userQuery == 'quit':
             break
 
         # captures the case in which no option is matched and returns message to the user.
         else:
             errorString = " command not found, press 'help' to view the help menu."
-            errorMessage = "'" + userCommand + "'" + errorString
+            errorMessage = "'" + userQuery + "'" + errorString
             print(errorMessage)
--- search/socialMedia.py
+++ search/socialMedia.py
@@ -1,63 +1,66 @@
-# Searches in for accounts in specific social media *through google* (at least for now).
-# Facebook's url.
+# imports the google wrapper class defined in this directory.
 import search.google as google
 
-__FACEBOOK__ = "www.facebook.com"
-__LINKEDIN__ = "www.linkedin.com"
-__TWITTER__ = "www.twitter.com"
-__INSTAGRAM__ = "www.instagram.com"
-__REDDIT__ = "www.reddit.com"
 
+# Responsible for social media account collection of target user, company or domain.
+class SocialMedia(object):
+    # Flags intro used by the user to indicate an action.
+    __SH_FLAG__ = '-'
+    __LO_FLAG__ = '--'
 
-# performs facebook search for the given query.
-def facebookSearch(query):
-    query += " inurl:" + "\"" + __FACEBOOK__ + "\""
-    return google.googleSearch(query)
+    # Advanced google dork used for returning results including the provided url.
+    __INURL__ = " inurl:"
 
+    # Social media websites that are currently supported by Who-Dis.
+    __FACEBOOK__ = "www.facebook.com"
+    __LINKEDIN__ = "www.linkedin.com"
+    __TWITTER__ = "www.twitter.com"
+    __INSTAGRAM__ = "www.instagram.com"
+    __REDDIT__ = "www.reddit.com"
 
-# performs linkedin search for the given query.
-def linkedinSearch(query):
-    query += " inurl:" + "\"" + __LINKEDIN__ + "\""
-    return google.googleSearch(query)
-
-
-# performs twitter search for the given query.
-def twitterSearch(query):
-    query += " inurl:" + "\"" + __TWITTER__ + "\""
-    return google.googleSearch(query)
-
-
-# performs instagram search for the given query.
-def instagramSearch(query):
-    query += " inurl:" + "\"" + __INSTAGRAM__ + "\""
-    return google.googleSearch(query)
-
-
-# performs reddit search for the given query.
-def redditSearch(query):
-    query += " inurl:" + "\"" + __REDDIT__ + "\""
-    return google.googleSearch(query)
-
-
-# searches all social media at once returning recent posts about target.
-class SocialSearcher:
     # Uses social searcher website collecting posts on given keyword on all social media.
     __startString__ = 0
     # The length of the space character, used to remove trailing space at the end of parsing keyword.
     __spaceCharLength__ = 1
 
-    # Performs a search in www.who.is to capture information about a target domain.
-    def ssSearch(query):
-        # search string used in the address bar to perform search.
-        ssLink_1 = 'https://www.social-searcher.com/social-buzz/?q5=' + SocialSearcher._ssParse(query)
-        ssLink_2 = 'https://www.social-searcher.com/google-social-search/?q=' + SocialSearcher._ssParse(query) + '&fb=on&tw=on&gp=on&in=on&li=on&pi=on'
-        # returns a web page as a result of this search.
-        print(ssLink_1)
-        print(ssLink_2)
-        print()
+    # Parses the query to avoid the flag inclusion while performing google search.
+    def __parseQuery(query):
+        flagIndex = str(query).index(SocialMedia.__SH_FLAG__) or str(query).index(SocialMedia.__LO_FLAG__)
+        parsedQuery = query[SocialMedia.__startString__:flagIndex - SocialMedia.__spaceCharLength__]
+        return parsedQuery
 
-    # Parses the query by cutting the -p flag.
-    def _ssParse(query):
-        flagIndex = str(query).index('-') or str(query).index('--')
-        query = query[SocialSearcher.__startString__:flagIndex - SocialSearcher.__spaceCharLength__]
-        return query
+    # Performs social search through google using the advanced google dork inurl.
+    def retrieveAccounts(query, media):
+        # Parses the query to avoid the flag inclusion while performing google search.
+        parsedQuery = SocialMedia.__parseQuery(query)
+        searchQuery = parsedQuery + SocialMedia.__INURL__
+
+        # Searches in corresponding social media indicated by user.
+        if media == 'fb':
+            searchQuery += SocialMedia.__FACEBOOK__
+
+        elif media == 'ln':
+            searchQuery += SocialMedia.__LINKEDIN__
+
+        elif media == 'tw':
+            searchQuery += SocialMedia.__TWITTER__
+
+        elif media == 'in':
+            searchQuery += SocialMedia.__INSTAGRAM__
+
+        elif media == 're':
+            searchQuery += SocialMedia.__REDDIT__
+
+        return google.googleSearch(query)
+
+    # Used to retrieve posts about target from all available social media websites.
+    # TODO needs the space function for name+surname form
+    # def retrievePosts(query):
+    #     parsedQuery = SocialMedia.__parseQuery(query)
+    #     # search string used in the address bar to perform search.
+    #     postsLink_1 = 'https://www.social-searcher.com/social-buzz/?q5=' + parsedQuery
+    #     postsLink_2 = 'https://www.social-searcher.com/google-social-search/?q=' + parsedQuery + '&fb=on&tw=on&gp=on&in=on&li=on&pi=on'
+    #     # returns a web page as a result of this search.
+    #     print(postsLink_1)
+    #     print(postsLink_2)
+    #     print()
