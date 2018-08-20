96b3c6a3b0ecaf73739653ac2cf73809a8cd3f22
jkatsioloudes <jo.katsioloudes@gmail.com>
2018-08-20 23:30:25 +0100

    Further welcome messages for random pick

--- intro.py
+++ intro.py
@@ -1,4 +1,15 @@
 # Welcome screen of the program.
+def version():
+    print("""
+
+Version 1.0 - Written by Joseph Katsioloudes, 2017.
+
+Please press 'help' for user manual and options.
+Press exit or quit to exit the program
+Please refer to 'Issues' section in GitHub for any problems you may have.
+""")
+
+
 def welcomeMessage_1():
     print("""
 
@@ -14,17 +25,13 @@ def welcomeMessage_1():
 ##                                                                       ## 
 ##                                                                       ##
 ###########################################################################
-
-Version 1.0 - Written by Joseph Katsioloudes, 2017.
-
-Please press 'help' for user manual and options.
-Press exit or quit to exit the program
-Please refer to 'Issues' section in GitHub for any problems you may have.
 """)
+    version()
 
 
 def welcomeMessage_2():
     print("""
+
 #####################################################################################################################################
 ##                                                                                                                                 ##
 ##                                                                                                                                 ##
@@ -53,3 +60,65 @@ def welcomeMessage_2():
 ##                                                                                                                                 ##
 #####################################################################################################################################  
 """)
+    version()
+
+
+def welcomeMessage_3():
+    print("""
+
+    
+    $$$$$$$$\ $$\       $$\                         
+    $$  _____|$$ |      $$ |                        
+    $$ |      $$$$$$$\  $$ | $$$$$$\  $$$$$$\$$$$\  
+    $$$$$\    $$  __$$\ $$ |$$  __$$\ $$  _$$  _$$\ 
+    $$  __|   $$ |  $$ |$$ |$$$$$$$$ |$$ / $$ / $$ |
+    $$ |      $$ |  $$ |$$ |$$   ____|$$ | $$ | $$ |
+    $$$$$$$$\ $$$$$$$  |$$ |\$$$$$$$\ $$ | $$ | $$ |
+    \________|\_______/ \__| \_______|\__| \__| \__|    
+   
+""")
+    version()
+
+
+def welcomeMessage_4():
+    print("""
+    
+     .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
+    | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+    | |  _________   | || |   ______     | || |   _____      | || |  _________   | || | ____    ____ | |
+    | | |_   ___  |  | || |  |_   _ \    | || |  |_   _|     | || | |_   ___  |  | || ||_   \  /   _|| |
+    | |   | |_  \_|  | || |    | |_) |   | || |    | |       | || |   | |_  \_|  | || |  |   \/   |  | |
+    | |   |  _|  _   | || |    |  __'.   | || |    | |   _   | || |   |  _|  _   | || |  | |\  /| |  | |
+    | |  _| |___/ |  | || |   _| |__) |  | || |   _| |__/ |  | || |  _| |___/ |  | || | _| |_\/_| |_ | |
+    | | |_________|  | || |  |_______/   | || |  |________|  | || | |_________|  | || ||_____||_____|| |
+    | |              | || |              | || |              | || |              | || |              | |
+    | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+    '----------------'  '----------------'  '----------------'  '----------------'  '----------------'
+    
+""")
+    version()
+
+
+def welcomeMessage_5():
+    print("""
+    
+                        bbbbbbbb                                                                
+  EEEEEEEEEEEEEEEEEEEEEEb::::::b            lllllll                                             
+  E::::::::::::::::::::Eb::::::b            l:::::l                                             
+  E::::::::::::::::::::Eb::::::b            l:::::l                                             
+  EE::::::EEEEEEEEE::::E b:::::b            l:::::l                                             
+    E:::::E       EEEEEE b:::::bbbbbbbbb     l::::l     eeeeeeeeeeee       mmmmmmm    mmmmmmm   
+    E:::::E              b::::::::::::::bb   l::::l   ee::::::::::::ee   mm:::::::m  m:::::::mm 
+    E::::::EEEEEEEEEE    b::::::::::::::::b  l::::l  e::::::eeeee:::::eem::::::::::mm::::::::::m
+    E:::::::::::::::E    b:::::bbbbb:::::::b l::::l e::::::e     e:::::em::::::::::::::::::::::m
+    E:::::::::::::::E    b:::::b    b::::::b l::::l e:::::::eeeee::::::em:::::mmm::::::mmm:::::m
+    E::::::EEEEEEEEEE    b:::::b     b:::::b l::::l e:::::::::::::::::e m::::m   m::::m   m::::m
+    E:::::E              b:::::b     b:::::b l::::l e::::::eeeeeeeeeee  m::::m   m::::m   m::::m
+    E:::::E       EEEEEE b:::::b     b:::::b l::::l e:::::::e           m::::m   m::::m   m::::m
+  EE::::::EEEEEEEE:::::E b:::::bbbbbb::::::bl::::::le::::::::e          m::::m   m::::m   m::::m
+  E::::::::::::::::::::E b::::::::::::::::b l::::::l e::::::::eeeeeeee  m::::m   m::::m   m::::m
+  E::::::::::::::::::::E b:::::::::::::::b  l::::::l  ee:::::::::::::e  m::::m   m::::m   m::::m
+  EEEEEEEEEEEEEEEEEEEEEE bbbbbbbbbbbbbbbb   llllllll    eeeeeeeeeeeeee  mmmmmm   mmmmmm   mmmmmm
+
+""")
+    version()
